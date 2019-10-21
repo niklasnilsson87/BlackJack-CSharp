e150f8d79fc7cf1e187554da197fb22c73b2af22
Niklas Nilsson <nn222ia@student.lnu.se>
2019-10-21 09:12:02 +0200

    adding code according to sequensediagram

--- model/rules/AmericanNewGameStrategy.cs
+++ model/rules/AmericanNewGameStrategy.cs
@@ -5,29 +5,29 @@ using System.Text;
 
 namespace BlackJack.model.rules
 {
-    class AmericanNewGameStrategy : INewGameStrategy
+  class AmericanNewGameStrategy : INewGameStrategy
+  {
+    public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
     {
-        public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
-        {
-            Card c;
+      Card c;
 
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
+      c = a_deck.GetCard();
+      c.Show(true);
+      a_player.DealCard(c);
 
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_dealer.DealCard(c);
+      c = a_deck.GetCard();
+      c.Show(true);
+      a_dealer.DealCard(c);
 
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
+      c = a_deck.GetCard();
+      c.Show(true);
+      a_player.DealCard(c);
 
-            c = a_deck.GetCard();
-            c.Show(false);
-            a_dealer.DealCard(c);
+      c = a_deck.GetCard();
+      c.Show(false);
+      a_dealer.DealCard(c);
 
-            return true;
-        }
+      return true;
     }
+  }
 }
